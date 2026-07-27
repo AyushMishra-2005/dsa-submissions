52c4cd32d0ba7eb5b5a129260163158933136dc3
Ayush Mishra <ayushmishra9449@gmail.com>
2026-07-27 18:31:00 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int ele: nums){
+            set.add(ele);
+        }
+
+        if(set.size() == nums.length){
+            return false;
+        }
+
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
